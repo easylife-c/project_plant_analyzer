81fee07b97b936476177f85c166dc8e5cb993a7c
easylife-c <134291114+easylife-c@users.noreply.github.com>
2025-02-13 10:38:48 +0700

    Update plant_api.py

--- plant_api.py
+++ plant_api.py
@@ -8,7 +8,7 @@ PLANT_ID_API_KEY = config.PLANT_ID_API_KEY
 PLANT_ID_URL = "https://plant.id/api/v3/health_assessment"
 
 def encode_image(image_path):
-    
+    #encode image into base64 strings
     try:
         with open(image_path, "rb") as image_file:
             base64_bytes=image_file.read()
@@ -25,7 +25,7 @@ def identify_plant(image_path):
     """Sends the image to the Plant.id API and returns the identification result."""
     encoded_image = encode_image(image_path) 
 
-    # Build the payload as per the latest API guidelines.
+    # Build the payload 
     payload = json.dumps({
         "images":[encoded_image],
         "latitude": 49.207,
@@ -33,7 +33,7 @@ def identify_plant(image_path):
         "health":"only",
         
         
-        #"details":["description","treatment"]
+        
         })
 
     headers = {
@@ -62,21 +62,7 @@ def identify_plant(image_path):
         print("No diseases detected.")
 
     
-        #if response.status_code == 201:
-            #identification = response.json()
-            #print('is plant' if identification['result']['is_plant']['binary'] else 'is not plant')
-            #print(suggestion['name'])
-                #print(f'probability {suggestion["probability"]:.2%}')
-                #print(suggestion['details']['url'], suggestion['details']['common_names'])
-                #print()
-            #return suggestion
-        #else:
-            #print(f"❌ API Error {response.status_code}: {response.text}")
-            #return {"error": f"API returned {response.status_code}"}
-    #except requests.exceptions.RequestException as e:
-        #print(f"❌ Network Error: {e}")
-        #return {"error": "Network error"}
-
+        
 # For standalone testing of this module:
 if __name__ == "__main__":
     image_path = "test.jpg"  # Replace with your actual test image path
